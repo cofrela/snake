370605c90d4e7f562fcf4f3d0eca6e048f004e71
cofrela <cofrela@gmail.com>
2017-05-06 21:52:17 +0700

    Горизонтальные и вертикальные линии

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,49 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
-            Point p3 = new Point(7, 7, '%');
-            p3.Draw();
-
-            Point p4 = new Point(10, 9, '&');
-            p4.Draw();
-
-            Point p5 = new Point(13, 11, '@');
-            p5.Draw();
-
-            Point p6 = new Point(16, 13, '^');
-            p6.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
-            pList.Add(p5);
-            pList.Add(p6);
-
-            Console.ReadLine(); // нажатие кнопки enter для выхода
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+     
+            Console.ReadLine();
 
         }
     }
